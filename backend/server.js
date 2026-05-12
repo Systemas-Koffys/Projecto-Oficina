027b21b03ac55357da42eb83b1c29a87f9b4edf0
Kevin Flores <koffy61862629@gmail.com>
2026-05-12 00:46:56 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Configuración de conexión flexible (Docker o Local)
 const pool = mysql.createPool({
